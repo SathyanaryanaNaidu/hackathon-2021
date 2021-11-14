2f85fd8475692a0d9515160d6ceab1a010a7651c
Sathyanarayana K U <sathyanarayanaku@MacBook-Pro.local>
2021-11-14 16:55:34 +0530

    removed unnecessary

--- containers/LayoutContainer/LayoutContainer.js
+++ containers/LayoutContainer/LayoutContainer.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import layOutStyle from './LayoutContainer.style';
-import PodCard from 'components/PodCard';
 import ContainerWrap from '@/ui/Container';
 
 const LayoutContainer = ({ products }) => {
   const { results = [] } = products;
   return (
     <ContainerWrap>
-      {results.map(
-        (
-          { isBestSeller, brand, displayName, mediaUrls, productId, variants },
-          index
-        ) => {
-          if (index > 30 || (!mediaUrls && !mediaUrls[0]))
-            return <React.Fragment />;
-          return (
-            <PodCard
-              key={`${productId}-${index}`}
-              badge={isBestSeller && 'BestSeller'}
-              brandName={brand}
-              productName={displayName}
-              description={displayName}
-              media={`https://falabella.scene7.com/is/image/Falabella/${productId}?wid=480&hei=480&qlt=70`}
-              promoted={index < 2}
-              variants={variants}
-            />
-          );
-        }
-      )}
       <style jsx>{layOutStyle}</style>
     </ContainerWrap>
   );
